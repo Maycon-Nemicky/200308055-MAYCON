9acdb6b27b0321dbea1a7803879a2218c69ec307
Maycon-Nemicky <89670188+Maycon-Nemicky@users.noreply.github.com>
2021-11-18 11:23:14 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>HOME</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Maycon Silva</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Estamos aqui</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="quemsomos.php">Quem somos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contato.php">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="localizaco.php">Localização</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">|| Venha fazer parte da nossa comunidade</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid"></div>
+<div class="p-3 mb-2 bg-primary text-white">
+   <h2>Aqui voçê verá todos os projetos já feitos por mim</h2>
+</div>
+</div>
+
+<img src="linha.jpg" alt="listra" style="height: 1700px;">
+
+
+<div class="container" style="transform: translate(-50px, -1600px)">
+<div class="card mb-3" style="max-width: 580px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="foto1.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Sistema de cadastro de pacientes</h5>
+        <p class="card-text">Sistema feito para clinica odontológica, com uma tabela onde fica listado todos os pacientes já cadastrados</p>
+        <p class="card-text">A tela principal (lista de pacientes) é formada por 4 botões: editar, cadastrar, excluir e dados;</p>
+        <p class="card-text">A tela do formulário de perguntas consiste em varias questões que servem como dados do paciente, como por exemplo: nome, email, telefone, etc...</p>
+
+        <a href="pj1.php"><button class="btn btn-primary btn-lg">ver detalhes</button></a>
+        
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="container" style="transform: translate(-50px, -1600px)">
+<div class="card mb-3" style="max-width: 580px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="foto2.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Sistema de gerenciamento de estoques</h5>
+        <p class="card-text">Este sistema contém um gereciamento de estoque bem mais que detalhado</p>
+        <p class="card-text">A tela principal (lista de produtos) é formada por vários botões: editar produto, cadastrar produto, excluir produto dados do devido produto;</p>
+        <p class="card-text">Nesta mesma tela fica toda uma lista de todos os produtos cadastrados no sistema com um controle de estoque rigoroso e automatizado</p>
+
+        <a href="pj2.php"><button class="btn btn-primary btn-lg">ver detalhes</button></a>
+        
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="container" style="transform: translate(-50px, -1600px)">
+<div class="card mb-3" style="max-width: 580px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="foto3.jfif" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Sistema de gerenciamento escolar</h5>
+        <p class="card-text">Este sistema contém um gereciamento de estoque bem mais que detalhado</p>
+        <p class="card-text">A tela principal (lista de produtos) é formada por vários botões: editar produto, cadastrar produto, excluir produto dados do devido produto;</p>
+        <p class="card-text">Nesta mesma tela fica toda uma lista de todos os produtos cadastrados no sistema com um controle de estoque rigoroso e automatizado</p>
+
+        <a href="pj3.php"><button class="btn btn-primary btn-lg">ver detalhes</button></a>
+        
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<div class="container" style="background-color: red; transform: translate(-50px, -1250px)">
+<div class="card mb-3" style="max-width: 1100px;">
+  <div class="row g-0">
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Softwares</h5>
+        <p class="card-text">Todo software é construido a partir de linguagens de programação, mas, o que pode ser um diferençial no seu software é a sua criatividade e imaginação...</p>
+        <p class="card-text">Esses Softwares vem de um vasto mundo de possibilidades pensados por nós desenvolvedores, para que possamos tornar a vida de tudo e de todos melhor.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+</body>
+</html>

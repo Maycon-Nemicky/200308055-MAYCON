a01d9fd2fcfc68f8d30c4e49982d234690281eaf
Maycon-Nemicky <89670188+Maycon-Nemicky@users.noreply.github.com>
2021-11-18 11:30:29 -0300

    pagina contato

---
+++ contato.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Maycon Silva</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Estamos aqui</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="quemsomos.php">Quem somos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="contato.php">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="localizacao.php">Localização</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">|| Venha fazer parte da nossa comunidade</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container-fluid"></div>
+<div class="p-3 mb-2 bg-primary text-white">
+   <h2>Página de contato</h2>
+</div>
+</div>
+
+<div class="container" style="background-color: red; font-size:30px; font-family: Sans-serif; color: white;">
+  <div class="row">
+    <div class="col-6 col-sm-4">E-mail</div>
+    <div class="col-6 col-sm-4">mayconnemicky@gmail.com</div>
+
+    <!-- Force next columns to break to new line at md breakpoint and up -->
+    <div class="w-100 d-none d-md-block"></div>
+
+    <div class="col-6 col-sm-4">Telefone</div>
+    <div class="col-6 col-sm-4">(63) 99975-9188</div>
+  </div>
+</div>
+
+<br><br><br><br><br>
+
+
+<div class="container" style="background-color: blue;">
+<div class="card mb-3" style="max-width: 1100px;">
+  <div class="row g-0">
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Meus Contatos</h5>
+        <p class="card-text">Envie-me um email ou acesse meu telefone de whatsapp caso queira comentar algo sobre os produtos disponiveis no site</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>

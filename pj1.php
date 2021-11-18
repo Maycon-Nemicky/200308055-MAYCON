c1f23debece58e6bd3e92d909251f1d3810f9134
Maycon-Nemicky <89670188+Maycon-Nemicky@users.noreply.github.com>
2021-11-18 11:25:26 -0300

    projetos

---
+++ pj1.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Maycon Silva</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Estamos aqui</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="home.php">Home>>cadastro de pacientes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="quemsomos.php">Quem somos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contato.php">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="localizaco.php">Localização</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">|| Venha fazer parte da nossa comunidade</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid"></div>
+<div class="p-3 mb-2 bg-primary text-white">
+   <h2 >Sistema de cadastro de pacientes</h2>
+</div>
+</div>
+
+   
+   <div class="container form-control" style="font-family: Oswald;">
+      
+      <h3><b> Características do sistema</b></h3>
+      <ul>
+         <li>
+            <h4>Interface gráfica totalmente moderna e atualizada com o tempo</h4>
+         </li>
+         <li>
+            <h4>Contém 6 botões no total: cadastrar, editar, excluir, dados, salvar e cancelar</h4>
+         </li>
+         <li>
+            <h4>contém um menu na vertical com todas as opções que um dentista precisa como: odontometria do paciente ou lichas utilizadas no procedimento, escolha de dentes para trabalhar etc...</h4>
+         </li>
+         <li>
+            <h4>Cores leves para não irritar os olhos</h4>
+         </li>
+         <li>
+            <h4>Formulário com todas informações necessárias para obtenção de dados do paciente</h4>
+         </li>
+      </ul>
+   </div>
+   <br>
+<div id="branco" style="background-color: red; width: 100%; text-align:center;color:white; font-size:18px"> 
+   <p>se voçê quer obter o seu produto a partir deste site, porfavor entre em contato comigo pelo email ou telefone no menu [contato]</p>
+</div>
+   <div class="container form-control" style="font-family: Oswald;">
+      <h3> <b>linguagens utilizadas</b></h3>
+      <ul>
+         <li>
+            <h4>HTML</h4>
+         </li>
+         <li><h4>CSS</h4></li>
+         <li><h4>PHP</h4></li>
+         <li><h4>JAVA SCRIPT</h4></li>
+      </ul>
+   </div>
+   <div class="container form-control" style="font-family: Oswald;">
+      <h3> <b>Frameworks utilizadas</b></h3>
+      <ul>
+         <li>
+            <h4>Bootstrap</h4>
+         </li>
+      </ul>
+   </div>
+   <br><br>
+
+   
+</body>
+</html>
---
+++ pj3.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+   <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Maycon Silva</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="index.php">Estamos aqui</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="home.php">Home>>sistema de gerenciamento escolar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="quemsomos.php">Quem somos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contato.php">Contato</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="localizaco.php">Localização</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">|| Venha fazer parte da nossa comunidade</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid"></div>
+<div class="p-3 mb-2 bg-primary text-white">
+   <h2 >Sistema de gerenciamento escolar</h2>
+</div>
+</div>
+
+   
+   <div class="container form-control" style="font-family: Oswald;">
+      
+      <h3><b> Características do sistema</b></h3>
+      <ul>
+         <li>
+            <h4>Interface gráfica adaptável a quem está usando o sistema com uma paleta de cores para definir a cor do sistema como um todo</h4>
+         </li>
+         <li>
+            <h4>contém um menu na horizontal com todas as funções em icones 
+               como por exemplo: dados do aluno, notas bimestrais, disciplinas a serem estudadas, disciplinas reprovadas, atividades pendentes, atividades conluidas, serviços de comunicação com professores, fóruns avaliativos abertos/fechados
+            </h4>
+         </li>
+         <li>
+            <h4>Cores moderadas para dar mais destaque no designe</h4>
+         </li>
+         <li>
+            <h4>As mesmas funções do menu horizontal estão na interface listadas uma abaixo da outra para melhor entendimento de tudo</h4>
+         </li>
+         <li>
+            <h4>Incluido um banco de dados no sistema para não haver perdas de conteudo</h4>
+         </li>
+      </ul>
+   </div>
+   <br>
+<div id="branco" style="background-color: red; width: 100%; text-align:center;color:white; font-size:18px"> 
+   <p>se voçê quer obter o seu produto a partir deste site, porfavor entre em contato comigo pelo email ou telefone no menu [contato]</p>
+</div>
+   <div class="container form-control" style="font-family: Oswald;">
+      <h3> <b>linguagens utilizadas</b></h3>
+      <ul>
+         <li>
+            <h4>HTML</h4>
+         </li>
+         <li><h4>CSS</h4></li>
+         <li><h4>PHP</h4></li>
+         <li><h4>JAVA SCRIPT</h4></li>
+         <li><h4>JAVA</h4></li>
+      </ul>
+   </div>
+   <div class="container form-control" style="font-family: Oswald;">
+      <h3> <b>Frameworks utilizadas</b></h3>
+      <ul>
+         <li>
+            <h4>Bootstrap</h4>
+         </li>
+      </ul>
+   </div>
+   <br><br>
+
+   
+</body>
+</html>
